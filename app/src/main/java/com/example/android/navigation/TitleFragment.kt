fb471d09d93e31d0de432c944c72ea56e758ecac
Samet Çağrı AKTEPE <blackorsaktepe@gmail.com>
2022-05-01 19:38:55 +0300

    Adding Safe Arguments

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -22,8 +22,9 @@ class TitleFragment : Fragment() {
     ): View? {
         val binding: FragmentTitleBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
-        binding.playButton.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment2))
+        binding.playButton.setOnClickListener { v: View ->
+            v.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment2())
+        }
         setHasOptionsMenu(true)
         return binding.root
     }
